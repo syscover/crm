6dbbb936da1e451253b9bd165ee9edf4c3eba105
Jose Carlos <cpalacin@syscover.com>
2015-07-09 13:41:48 +0200

    upgrade to laravel 5.1

--- src/Syscover/Crm/Models/Family.php
+++ src/Syscover/Crm/Models/Family.php
@@ -12,11 +12,11 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
-use Syscover\Pulsar\Traits\ModelTrait;
+use Syscover\Pulsar\Traits\TraitModel;
 
 class Family extends Model {
 
-    use ModelTrait;
+    use TraitModel;
 
 	protected $table        = '012_300_family';
     protected $primaryKey   = 'id_300';
