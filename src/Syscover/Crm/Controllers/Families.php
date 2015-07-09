1cebdbdf363fc501926bb31da11a99c382496524
Jose Carlos <cpalacin@syscover.com>
2015-07-09 13:37:41 +0200

    upgrade to laravel 5.1

--- src/Syscover/Crm/Controllers/Families.php
+++ src/Syscover/Crm/Controllers/Families.php
@@ -12,12 +12,12 @@
 
 use Illuminate\Support\Facades\Request;
 use Syscover\Pulsar\Controllers\Controller;
-use Syscover\Pulsar\Traits\ControllerTrait;
+use Syscover\Pulsar\Traits\TraitController;
 use Syscover\Crm\Models\Family;
 
 class Families extends Controller {
 
-    use ControllerTrait;
+    use TraitController;
 
     protected $routeSuffix  = 'CrmFamily';
     protected $folder       = 'families';
