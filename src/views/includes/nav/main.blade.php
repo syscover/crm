8ac0c205e75b49ac2bcdc706e014d0ff7dd32609
Jose Carlos <cpalacin@syscover.com>
2015-07-27 20:11:33 +0200

    session update

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -1,7 +1,7 @@
         <li{!! Miscellaneous::setCurrentOpenPage(['crm-family']) !!}>
             <a href="javascript:void(0);"><i class="icomoon-icon-users"></i>CRM</a>
             <ul class="sub-menu">
-                @if(Session::get('userAcl')->isAllowed(Auth::user()->profile_010, 'crm-family', 'access'))
+                @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'crm-family', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('crm-family') !!}><a href="{{ route('CrmFamily') }}"><i class="icomoon-icon-grid"></i>{{ trans_choice('pulsar::pulsar.family', 2) }}</a></li>
                 @endif
             </ul>
