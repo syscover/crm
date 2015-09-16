9d196dee9c90a5d88fedf0419821bac92039ad4f
Jose Carlos <cpalacin@syscover.com>
2015-09-17 00:41:24 +0200

    crm

---
+++ src/database/dev/2015_04_01_120010_crm_create_table_customer.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrmCreateTableCustomer extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('012_300_family', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id_300')->unsigned();
+            $table->string('name_300', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('012_300_family');
+    }
+
+}
