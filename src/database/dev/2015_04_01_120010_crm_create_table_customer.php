ba05cc4452f98e584456f3091e6c4f26b439c981
Jose Carlos <cpalacin@syscover.com>
2015-10-01 00:52:08 +0200

    database

--- src/database/dev/2015_04_01_120010_crm_create_table_customer.php
+++ src/database/dev/2015_04_01_120010_crm_create_table_customer.php
@@ -12,11 +12,28 @@ class CrmCreateTableCustomer extends Migration {
      */
     public function up()
     {
-        Schema::create('012_300_family', function(Blueprint $table)
+        Schema::create('012_301_customer', function(Blueprint $table)
         {
             $table->engine = 'InnoDB';
-            $table->increments('id_300')->unsigned();
-            $table->string('name_300', 50);
+            $table->increments('id_301')->unsigned();
+            $table->string('name_301', 100)->nullable();
+            $table->string('surname_301', 100)->nullable();
+            $table->string('email_301', 50)->nullable();
+            $table->integer('birthdate_301')->nullable();
+
+
+            // geolocation data
+            $table->string('country_301', 2);
+            $table->string('territorial_area_1_301', 6)->nullable();
+            $table->string('territorial_area_2_301', 10)->nullable();
+            $table->string('territorial_area_3_301', 10)->nullable();
+            $table->string('cp_301', 10)->nullable();
+            $table->string('locality_301', 100)->nullable();
+            $table->string('address_301', 150)->nullable();
+            $table->string('latitude_301', 50)->nullable();
+            $table->string('longitude_301', 50)->nullable();
+
+            $table->text('data_301')->nullable();
         });
     }
 
@@ -27,7 +44,7 @@ class CrmCreateTableCustomer extends Migration {
      */
     public function down()
     {
-        Schema::drop('012_300_family');
+        Schema::drop('012_301_customer');
     }
 
 }
