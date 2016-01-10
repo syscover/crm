21f54b69a70c63c64545c70824772beb4fd43249
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-01-10 14:28:53 +0100

    breadcrumbs on customer arm

---
+++ src/views/customer/breadcrumbs.blade.php
@@ -0,0 +1,8 @@
+<!-- crm::customer.breadcrumbs -->
+<li>
+    <a href="javascript:void(0)">{{ trans('crm::pulsar.package_name') }}</a>
+</li>
+<li class="current">
+    <a href="{{ route($routeSuffix) }}">{{ trans_choice($objectTrans, 2) }}</a>
+</li>
+<!-- /crm::customer.breadcrumbs -->
