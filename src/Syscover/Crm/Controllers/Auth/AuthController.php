bf23555212e2963a7c229d6c844aa4d3528df2f2
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-05 12:41:17 +0100

    auth controllers

---
+++ src/Syscover/Crm/Controllers/Auth/AuthController.php
@@ -0,0 +1,137 @@
+<?php namespace Syscover\Crm\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Pulsar\Support\Facades\Config;
+use Syscover\Pulsar\Libraries\AclLibrary;
+
+class AuthController extends Controller
+{
+
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
+
+	use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo;
+
+    /**
+     * Login route
+     *
+     * @var string
+     */
+    protected $loginPath;
+
+    /**
+     * Redirect route after logout
+     *
+     * @var string
+     */
+    protected $logoutPath;
+
+    /**
+     * Here you can customize your guard, this guar has to set in auth.php config
+     *
+     * @var string
+     */
+    protected $guard;
+
+
+	/**
+	 * Create a new authentication controller instance.
+	 */
+	public function __construct()
+	{
+        $this->redirectTo   = route('dashboard');
+        $this->loginPath    = route('getLogin');
+        $this->logoutPath   = route('getLogin');
+	}
+
+    /**
+     * Show the application login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogin()
+    {
+        return view('pulsar::auth.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $this->validate($request, [
+            'user_301' => 'required', 'password' => 'required',
+        ]);
+
+        $credentials = $request->only('user_301', 'password');
+
+        if(auth('crm')->attempt($credentials, $request->has('remember')))
+        {
+            // check if customer is confirmed
+            if (!auth('crm')->user()->confirmed_301)
+            {
+                auth('crm')->logout();
+
+                return redirect($this->loginPath)
+                    ->withInput($request->only('user_301', 'remember'))
+                    ->withErrors([
+                        'loginErrors' => 2
+                    ]);
+            }
+
+            // check if customer is active
+            if(!auth('crm')->user()->active_301)
+            {
+                auth('crm')->logout();
+
+                return redirect($this->loginPath)
+                    ->withInput($request->only('user_301', 'remember'))
+                    ->withErrors([
+                        'loginErrors' => 3
+                    ]);
+            }
+
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect($this->loginPath)
+            ->withInput($request->only('user_301', 'remember'))
+            ->withErrors([
+                'loginErrors' => 1
+            ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogout()
+    {
+        auth('crm')->logout();
+        session()->flush();
+
+        return redirect($this->logoutPath);
+    }
+}
---
+++ src/Syscover/Crm/Controllers/Auth/PasswordController.php
@@ -0,0 +1,201 @@
+<?php namespace Syscover\Crm\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Session;
+
+class PasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * The authentication guard that should be used.
+     *
+     * @var string
+     */
+    protected $guard = 'crm';
+
+    /**
+     * The password broker that should be used.
+     *
+     * @var string
+     */
+    protected $broker = 'crmPasswordBroker';
+
+    /**
+     * The view to reset email.
+     *
+     * @var string
+     */
+    protected $resetView = 'pulsar::auth.reset';
+
+    /**
+     * Subject of reset email.
+     *
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * Create a new password controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->subject      = trans('pulsar::pulsar.subject_password_reset');
+        $this->redirectPath = route('getLogin');
+    }
+
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postEmail(Request $request)
+    {
+        $this->validate($request, ['email_301' => 'required|email']);
+
+        $broker = $this->getBroker();
+
+        $response = Password::broker($broker)->sendResetLink($request->only('email_301'), function (Message $message) {
+            $message->subject($this->getEmailSubject());
+        });
+
+        switch ($response) {
+            case Password::RESET_LINK_SENT:
+                return response()->json([
+                    'success' => true
+                ]);
+
+            case Password::INVALID_USER:
+            default:
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid user'
+                ], 400);
+        }
+    }
+
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Http\Response
+     */
+    public function getReset(Request $request, $token = null)
+    {
+        if (is_null($token))
+            return $this->getEmail();
+
+        $email = $request->input('email_301');
+
+        if (property_exists($this, 'resetView'))
+            return view($this->resetView)->with(compact('token', 'email'));
+
+        if (view()->exists('auth.passwords.reset'))
+            return view('auth.passwords.reset')->with(compact('token', 'email'));
+
+        return view('auth.reset')->with(compact('token', 'email'));
+    }
+
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postReset(Request $request)
+    {
+        $this->validate($request, [
+            'token'         => 'required',
+            'email_301'     => 'required|email',
+            'password'      => 'required|confirmed|min:6',
+        ]);
+
+        $credentials = $request->only(
+            'email_301', 'password', 'password_confirmation', 'token'
+        );
+
+        $broker = $this->getBroker();
+
+        $response = Password::broker($broker)->reset($credentials, function ($user, $password) {
+            $this->resetPassword($user, $password);
+        });
+
+        switch ($response) {
+            case Password::PASSWORD_RESET:
+                return $this->getResetSuccessResponse('pulsar::passwords.reset');
+
+            default:
+                return $this->getResetFailureResponse($request, $response);
+        }
+    }
+
+    /**
+     * Get the response for after a successful password reset.
+     *
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getResetSuccessResponse($response)
+    {
+        Session::flash('status', trans($response));
+
+        return redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the response for after a failing password reset.
+     *
+     * @param  Request  $request
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getResetFailureResponse(Request $request, $response)
+    {
+        return redirect()->back()
+            ->withInput($request->only('email_301'))
+            ->withErrors(['email_301' => trans($response)]);
+    }
+
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password_301 = bcrypt($password);
+
+        $user->save();
+
+        // if redirect to user zone, we set new user on Auth
+        //Auth::guard($this->getGuard())->login($user);
+    }
+}
