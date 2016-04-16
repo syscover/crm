aea157c5da5f08f244dcdf94b7b94e2586326b98
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-16 12:07:04 +0200

    translations

--- src/lang/es/pulsar.php
+++ src/lang/es/pulsar.php
@@ -1,16 +1,5 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by the paginator library to build
-    | the pagination links. You're free to change them to anything you want.
-    | If you come up with something more exciting, let us know.
-    |
-    */
     'package_name'                          => 'CRM',
 ];
