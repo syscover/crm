4bf5287eeef25c2e7e302b2ef41d639c1a7ea37c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-07-16 22:06:50 +0200

    minor fix

--- src/database/seeds/CrmAttachmentMimeSeeder.php
+++ src/database/seeds/CrmAttachmentMimeSeeder.php
@@ -8,13 +8,13 @@ class CrmAttachmentMimeSeeder extends Seeder
     public function run()
     {
         AttachmentMime::insert([
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'image/jpeg'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'image/png'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'text/plain'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'application/pdf'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'application/msword'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'application/msexcel'],
-            ['resource_id_019' => 'cms-article', 'mime_019' => 'application/zip'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'image/jpeg'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'image/png'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'text/plain'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'application/pdf'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'application/msword'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'application/msexcel'],
+            ['resource_id_019' => 'crm-customer', 'mime_019' => 'application/zip'],
         ]);
     }
 }
