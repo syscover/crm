5fd21e444013306d394922e906bb20f6512f9285
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-07-19 16:54:25 +0200

    minor fix

--- src/database/migrations/updates/2009_01_01_000002_crm_updateV2.php
+++ src/database/migrations/updates/2009_01_01_000002_crm_updateV2.php
@@ -13,11 +13,11 @@ class CrmUpdateV2 extends Migration
 	 */
 	public function up()
 	{
-		if(!Schema::hasColumn('009_301_customer', 'lang_301'))
+		if(!Schema::hasColumn('009_301_customer', 'lang_id_301'))
 		{
 			Schema::table('009_301_customer', function ($table) {
-				$table->string('lang_301', 2)->default('es')->after('id_301');
-				$table->foreign('lang_301', 'fk06_012_301_customer')->references('id_001')->on('001_001_lang')
+				$table->string('lang_id_301', 2)->default('es')->after('id_301');
+				$table->foreign('lang_id_301', 'fk06_012_301_customer')->references('id_001')->on('001_001_lang')
 					->onDelete('restrict')->onUpdate('cascade');
 			});
 		}
