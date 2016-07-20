8e69913d4ee00be33f1818fca6a25f86aef0a26c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-07-20 17:54:44 +0200

    minor fix

--- src/Syscover/Crm/Libraries/CustomerLibrary.php
+++ src/Syscover/Crm/Libraries/CustomerLibrary.php
@@ -21,9 +21,11 @@ class CustomerLibrary
      * state_id_301 [stateId]
      * name_301 [name]
      * surname_301 [surname]
+     * avatar_301 [avatar]
      * birth_date_301 [birthDate]
      * email_301 [email]
      * phone_301 [phone]
+     * mobile_301 [mobile]
      * user_301 [user]
      * password_301 [password]
      * active_301 [active]
@@ -33,6 +35,8 @@ class CustomerLibrary
      * cp_301 [cp]
      * locality_301 [locality]
      * address_301 [address]
+     * latitude_301 [latitude]
+     * longitude_301 [longitude]
      *
      * @param   \Illuminate\Http\Request        $request
      * @return  \Syscover\Crm\Models\Customer   $customer
@@ -46,7 +50,7 @@ class CustomerLibrary
         $customer = Customer::create([
             'lang_id_301'               => $request->has('langId')? $request->input('langId') : null,
             'group_id_301'              => $request->has('groupId')? $request->input('groupId') : null,
-            'date_301'                  => $request->has('date')? $request->input('date') : date('U'),
+            'date_301'                  => $request->has('date')? \DateTime::createFromFormat(config('pulsar.datePattern'), $request->input('date'))->getTimestamp() : date('U'),
             'company_301'               => $request->has('company')? $request->input('company') : null,
             'tin_301'                   => $request->has('tin')? $request->input('tin') : null,
             'gender_id_301'             => $request->has('gender')? $request->input('gender') : null,
@@ -54,19 +58,24 @@ class CustomerLibrary
             'state_id_301'              => $request->has('stateId')? $request->input('stateId') : null,
             'name_301'                  => $request->has('name')? ucwords(strtolower($request->input('name'))) : null,
             'surname_301'               => $request->has('surname')? ucwords(strtolower($request->input('surname'))) : null,
+            'avatar_301'                => $request->has('avatar')? $request->input('avatar') : null,
             'birth_date_301'            => $request->has('birthDate')? \DateTime::createFromFormat(config('pulsar.datePattern'), $request->input('birthDate'))->getTimestamp() : null,
             'email_301'                 => strtolower($request->input('email')),
             'phone_301'                 => $request->has('phone')? $request->input('phone') : null,
+            'mobile_301'                => $request->has('mobile')? $request->input('mobile') : null,
             'user_301'                  => $request->has('user')? $request->input('user') : strtolower($request->input('email')),
-            'password_301'              => $request->has('password')? Hash::make($request->input('password')) : Miscellaneous::randomStr(8),
+            'password_301'              => $request->has('password')? Hash::make($request->input('password')) : Hash::make(Miscellaneous::randomStr(8)),
             'active_301'                => $request->has('active'),
             'confirmed_301'             => false,
             'country_id_301'            => $request->has('country')? $request->input('country') : null,
             'territorial_area_1_id_301' => $request->has('territorialArea1')? $request->input('territorialArea1') : null,
             'territorial_area_2_id_301' => $request->has('territorialArea2')? $request->input('territorialArea2') : null,
+            'territorial_area_3_id_301' => $request->has('territorialArea3')? $request->input('territorialArea3') : null,
             'cp_301'                    => $request->has('cp')? $request->input('cp') : null,
             'locality_301'              => $request->has('locality')? ucfirst($request->input('locality')) : null,
-            'address_301'               => $request->has('address')? $request->input('address') : null
+            'address_301'               => $request->has('address')? $request->input('address') : null,
+            'latitude_301'              => $request->has('latitude')? $request->input('latitude') : null,
+            'longitude_301'             => $request->has('longitude')? $request->input('longitude') : null
         ]);
 
         return $customer;
@@ -87,9 +96,11 @@ class CustomerLibrary
      * state_id_301 [stateId]
      * name_301 [name]
      * surname_301 [surname]
+     * avatar_301 [avatar]
      * birth_date_301 [birthDate]
      * email_301 [email]
      * phone_301 [phone]
+     * mobile_301 [mobile]
      * user_301 [user]
      * password_301 [password]
      * active_301 [active]
@@ -99,6 +110,8 @@ class CustomerLibrary
      * cp_301 [cp]
      * locality_301 [locality]
      * address_301 [address]
+     * latitude_301 [latitude]
+     * longitude_301 [longitude]
      *
      * @param   \Illuminate\Http\Request        $request
      * @return  \Syscover\Crm\Models\Customer   $customer
@@ -115,7 +128,7 @@ class CustomerLibrary
         Customer::where('id_301', $request->input('id'))->update([
             'lang_id_301'               => $request->has('langId')? $request->input('langId') : null,
             'group_id_301'              => $request->has('groupId')? $request->input('groupId') : null,
-            'date_301'                  => $request->has('date')? $request->input('date') : date('U'),
+            'date_301'                  => $request->has('date')? \DateTime::createFromFormat(config('pulsar.datePattern'), $request->input('date'))->getTimestamp() : date('U'),
             'company_301'               => $request->has('company')? $request->input('company') : null,
             'tin_301'                   => $request->has('tin')? $request->input('tin') : null,
             'gender_id_301'             => $request->has('gender')? $request->input('gender') : null,
@@ -123,9 +136,11 @@ class CustomerLibrary
             'state_id_301'              => $request->has('stateId')? $request->input('stateId') : null,
             'name_301'                  => $request->has('name')? ucwords(strtolower($request->input('name'))) : null,
             'surname_301'               => $request->has('surname')? ucwords(strtolower($request->input('surname'))) : null,
+            'avatar_301'                => $request->has('avatar')? $request->input('avatar') : null,
             'birth_date_301'            => $request->has('birthDate')? \DateTime::createFromFormat(config('pulsar.datePattern'), $request->input('birthDate'))->getTimestamp() : null,
             'email_301'                 => strtolower($request->input('email')),
             'phone_301'                 => $request->has('phone')? $request->input('phone') : null,
+            'mobile_301'                => $request->has('mobile')? $request->input('mobile') : null,
             'user_301'                  => $request->has('user')? $request->input('user') : strtolower($request->input('email')),
             'password_301'              => $request->has('password')? Hash::make($request->input('password')) : Miscellaneous::randomStr(8),
             'active_301'                => $request->has('active'),
@@ -133,9 +148,12 @@ class CustomerLibrary
             'country_id_301'            => $request->has('country')? $request->input('country') : null,
             'territorial_area_1_id_301' => $request->has('territorialArea1')? $request->input('territorialArea1') : null,
             'territorial_area_2_id_301' => $request->has('territorialArea2')? $request->input('territorialArea2') : null,
+            'territorial_area_3_id_301' => $request->has('territorialArea3')? $request->input('territorialArea3') : null,
             'cp_301'                    => $request->has('cp')? $request->input('cp') : null,
             'locality_301'              => $request->has('locality')? ucfirst($request->input('locality')) : null,
-            'address_301'               => $request->has('address')? $request->input('address') : null
+            'address_301'               => $request->has('address')? $request->input('address') : null,
+            'latitude_301'              => $request->has('latitude')? $request->input('latitude') : null,
+            'longitude_301'             => $request->has('longitude')? $request->input('longitude') : null
         ]);
 
         $customer = Customer::builder()->find($request->input('id'));
